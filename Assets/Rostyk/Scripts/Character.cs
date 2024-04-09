ed6ee36951c2d11a83637960605c0b160d4dd4dd
5antUA <rostykkashper2@gmail.com>
2024-04-09 22:08:44 +0300

    Adding class Character

---
+++ Assets/Rostyk/Scripts/Character.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+
+    [Header("\t CHARACTER PROPERTIES")]
+    [Space]
+    public float WalkSpeed;                                    // скорость ходьбы
+    public float SprintSpeed;                                  // скорость бега
+    public float MaxHealth;                                    // здоровье игрока
+    public float Damage;
+    public float Armor;                                        // количество брони
+
+    public bool isDead => MaxHealth <= 0;                      // если умер
+
+
+    public void TakeDamage(float damage)
+    {
+        this.MaxHealth -= damage;
+    }
+}
