e1008fdcd918a93a0b5fd9b2618bbd949e3df876
5antUA <rostykkashper2@gmail.com>
2024-04-09 22:12:42 +0300

    adding player movement logic

--- Assets/Rostyk/Scripts/Player.cs
+++ Assets/Rostyk/Scripts/Player.cs
@@ -4,6 +4,84 @@ using UnityEngine;
 
 public class Player : Character
 {
+    [Header("\t PLAYER SPECIAL")]
+    [Space]
 
 
+    #region Monster properties
+    // INSPECTOR
+    public float JumpForce;                                // сила прыжка
+    public float MaxVelocity;                              // ? ? ?
+
+    // NOT INSPECTOR                                        
+    private bool isSprinting;                              // если бежит
+    private bool inGround;                                 // если на земле
+
+    // COMPONENTS
+    private Rigidbody _rb;
+    #endregion
+
+
+
+    #region Management
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        _rb = GetComponent<Rigidbody>();
+
+        inGround = true;
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+    #endregion
+
+    #region Movement/Rotation
+    // Логика движения персонажа
+    private void Movement()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(moveX, 0, moveZ);
+
+        // Проверка на ходьбу
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            // Проверка на бег
+            if (Input.GetKey(KeyCode.LeftShift) && Input.GetAxis("Vertical") > 0)
+            {
+                direction = transform.TransformDirection(direction) * SprintSpeed;
+                isSprinting = true;
+            }
+            else
+            {
+                direction = transform.TransformDirection(direction) * WalkSpeed;
+                isSprinting = false;
+            }
+
+            // Логика движения
+            Vector3 velocity = _rb.velocity;
+            Vector3 velocityChange = (direction - velocity);
+            velocityChange.x = Mathf.Clamp(velocityChange.x, -MaxVelocity, MaxVelocity);
+            velocityChange.z = Mathf.Clamp(velocityChange.z, -MaxVelocity, MaxVelocity);
+            velocityChange.y = 0;
+
+            _rb.AddForce(velocityChange, ForceMode.VelocityChange);
+        }
+        else
+        {
+            isSprinting = false;
+            return;
+        }
+    }
+    #endregion
 }
