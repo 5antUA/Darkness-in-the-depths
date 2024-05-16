e3ad9933b17c24b035d10ed1dbb5983f0d7ec3b5
Mah0rka <131210581+Mah0rka@users.noreply.github.com>
2024-05-16 18:51:12 +0300

    торохи фіксів

--- Assets/Vladislav/Prefabs/Mobs/Boss/scripts/BossDamaggerControl.cs
+++ Assets/Vladislav/Prefabs/Mobs/Boss/scripts/BossDamaggerControl.cs
@@ -1,10 +1,9 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BossDamaggerControl : MobDamager
 {
-    public float corutineTime=0.5f;
+    public float corutineTime = 0.5f;
     private void Update()
     {
         if (animator.GetBool("Attack") == true)
@@ -34,9 +33,10 @@ public class BossDamaggerControl : MobDamager
         }
     }
 
-    private IEnumerator PushTime(Player enemy) {
+    private IEnumerator PushTime(Player enemy)
+    {
         yield return new WaitForSeconds(corutineTime);
         enemy.GetComponent<CharacterController>().
-                Move(new Vector3(enemy.transform.position.x, enemy.transform.position.y + 3, enemy.transform.position.z - 5) * Time.deltaTime * 7);
+                Move(new Vector3(enemy.transform.localPosition.x , enemy.transform.localPosition.y + 2, enemy.transform.localPosition.z ) * Time.deltaTime);
     }
 }
