21c086b4137c2ba3668472816ef226a514638b82
5antUA <rostykkashper2@gmail.com>
2024-05-24 12:49:54 +0300

    solve conf 2

--- Assets/Rostyk/Scripts/Z/ObjectFactory.cs
+++ Assets/Rostyk/Scripts/Z/ObjectFactory.cs
@@ -37,7 +37,11 @@ public class ObjectFactory : MonoBehaviour
         }
     }
 
+<<<<<<< HEAD
+    //private void SavePlayerPosition()
+=======
     //private void SaveGame()
+>>>>>>> 3d946fd (Начало создания фабрики объектов (сохранения и загрузка))
     //{
     //    for (int i = 0; i < InstatiateObjects.Count; i++)
     //    {
