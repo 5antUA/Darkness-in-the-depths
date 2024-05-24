631cbb42c945f4e018cb76183036e7b725c1a45a
5antUA <rostykkashper2@gmail.com>
2024-05-24 09:21:24 +0300

    фикс

---
+++ Assets/Rostyk/Scripts/SavedData/EnemiesData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SavedData
+{
+    [System.Serializable]
+    public class EnemiesData
+    {
+        public List<EnemyData> EnemiesList = new List<EnemyData>();
+
+        public void Save(List<GameObject> Enemies)
+        {
+            foreach (var enemy in Enemies)
+            {
+
+            }
+        }
+
+        public void Load()
+        {
+
+        }
+    }
+
+    [System.Serializable]
+    public class EnemyData
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+
+        public EnemyData(Vector3 pos, Quaternion rot)
+        {
+            position = pos;
+            rotation = rot;
+        }
+    }
+}
