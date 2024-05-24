f2c109fd6eb1bfc727a8ac79ffa4cfdce00bd122
5antUA <rostykkashper2@gmail.com>
2024-05-24 22:01:40 +0300

    Добавлен новый персонаж + обнова префаба игрока

---
+++ Assets/Rostyk/Scripts/SavedData/WeaponData.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+
+
+namespace SavedData
+{
+    [System.Serializable]
+    public class WeaponData
+    {
+        private const string KEY = "WeaponData";
+        public bool[] Weapons;
+
+        public WeaponData()
+        {
+            Weapons = new bool[2];
+            Weapons[0] = true;
+        }
+
+
+        #region Management
+        public void Save()
+        {
+            StorageService.Save(KEY, this);
+        }
+
+        public WeaponData Load()
+        {
+            try
+            {
+                var newData = StorageService.Load<WeaponData>(KEY);
+                return newData;
+            }
+            catch (FileNotFoundException)
+            {
+                Save();
+                return this;
+            }
+        }
+        #endregion
+    }
+}
