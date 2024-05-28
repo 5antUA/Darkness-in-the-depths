d7aae73ef869149aa06d15b19d45107c0bbc0fc2
5antUA <rostykkashper2@gmail.com>
2024-05-28 23:49:53 +0300

    Фикс звука в главном меню

--- Assets/Rostyk/Scripts/PlayerUI/MainMenu/Audio/PointerClick.cs
+++ Assets/Rostyk/Scripts/PlayerUI/MainMenu/Audio/PointerClick.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PointerClick : MonoBehaviour
 {
     [SerializeField] private AudioSource _AudioSource;
     [SerializeField] private AudioClip _AudioClip;
+    [SerializeField] private Button Button;
 
     public void AudioPlay()
     {
+        if (!Button.interactable)
+            return;
+
         _AudioSource.PlayOneShot(_AudioClip);
     }
 }
