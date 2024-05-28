70de3e087941afc83bbc13844e325a73479c421b
5antUA <rostykkashper2@gmail.com>
2024-05-29 00:03:36 +0300

    no message

--- Assets/Rostyk/Scripts/Triggers/DamagerTrigger.cs
+++ Assets/Rostyk/Scripts/Triggers/DamagerTrigger.cs
@@ -7,4 +7,12 @@ public class DamagerTrigger : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
             other.GetComponent<Player>().Health -= 1;
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            EventManager.ShowDamageScreen();
+        }
+    }
 }
