963dbd78b33d69b4ab0e50b1cb7c2672c22119de
5antUA <rostykkashper2@gmail.com>
2024-05-30 22:45:45 +0300

    Довідник майже зроблений

---
+++ Assets/Rostyk/Scripts/PlayerUI/GameMenu/BookButtons.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+public class BookButtons : MonoBehaviour
+{
+    [SerializeField] private GameObject SelfGameObject;
+    [SerializeField] private Transform AllTabs;
+
+    public void ActivateButton()
+    {
+        DeactivateTabs();
+        SelfGameObject.SetActive(true);
+    }
+
+    private void DeactivateTabs()
+    {
+        for (int i = 0; i < AllTabs.childCount; i++)
+        {
+            AllTabs.GetChild(i).gameObject.SetActive(false);
+        }
+    }
+}
