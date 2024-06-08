9c2178f357dc407bba3d552feff549b2a1063e92
Romario-Desantes <153783280+Romario-Desantes@users.noreply.github.com>
2024-06-08 18:47:17 +0300

    Добавив дверку без ключа

---
+++ Assets/Scripts FB/SimpleDoor.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleDoor : MonoBehaviour
+{
+    private Animator anim;
+    public LayerMask layer;
+    public float distance;
+    private Camera cam;
+    private bool isOpen;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        cam = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Camera>();
+        isOpen = false;
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(cam.transform .position, cam.transform.forward);
+        if(Physics.Raycast(ray, out hit, distance, layer))
+        {
+            if (Input.GetKeyDown(KeyCode.E) && !isOpen)
+            {
+                anim.SetBool("isOpen", true);
+                isOpen = true;
+            }
+            else if (Input.GetKeyDown(KeyCode.E) && isOpen)
+            {
+                isOpen = false;
+                anim.SetBool("isOpen", false);
+            }
+        }
+    }
+}
